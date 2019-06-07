836c8d8af7d4a1b9fe6dcee873e19da425a806f4
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-06-07 06:54:36 +0300

    Remove capistrano (#13)

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-lock "~> 3.11.0"
-
-set :application, "app"
-set :repo_url, "git@gitlab.com:dev1vitaly/reddit.git"
-set :deploy_to, "/home/deploy/app"
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-append :linked_files, "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads"
