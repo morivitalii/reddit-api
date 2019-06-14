1d93ad5d98a22d2ca2d05750d019780fe38abfba
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-06-14 18:44:04 +0300

    Remove column deletion_reasons_updated_at from subs table

---
+++ db/migrate/20190614154235_remove_deletion_reasons_updated_at_from_subs.rb
@@ -0,0 +1,5 @@
+class RemoveDeletionReasonsUpdatedAtFromSubs < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :subs, :deletion_reasons_updated_at, :datetime, null: false, default: -> { 'now()::timestamp' }
+  end
+end
