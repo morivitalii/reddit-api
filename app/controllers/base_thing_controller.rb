932f026b74344e24e4a18d3414993f1e9f7295bc
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-06-19 18:40:13 +0300

    Remove thing page not found page

--- app/controllers/base_thing_controller.rb
+++ app/controllers/base_thing_controller.rb
@@ -8,12 +8,4 @@ class BaseThingController < BaseSubController
   def set_thing
     @thing = @sub.things.find(params[:id])
   end
-
-  def page_not_found
-    if request.xhr?
-      head :not_found
-    else
-      render "things/page_not_found", status: :not_found
-    end
-  end
 end
