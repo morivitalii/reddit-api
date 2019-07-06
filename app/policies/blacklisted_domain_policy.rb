9c7f27f2b03c78ad4b2283524b980518cfaf1a2b
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-06 07:42:52 +0300

    Blacklisted domain policy

--- app/policies/blacklisted_domain_policy.rb
+++ app/policies/blacklisted_domain_policy.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-class BlacklistedDomainsPolicy < ApplicationPolicy
+class BlacklistedDomainPolicy < ApplicationPolicy
   def index?
     staff?
   end
 
+  alias search? index?
   alias new? index?
   alias create? index?
   alias confirm? index?
