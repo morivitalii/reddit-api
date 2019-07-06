52e8d5d5f918610e0544ad414c5f1ce8a58aedb7
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-06 08:25:43 +0300

    Deletion reason policy

--- app/policies/deletion_reason_policy.rb
+++ app/policies/deletion_reason_policy.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
-class DeletionReasonsPolicy < ApplicationPolicy
+class DeletionReasonPolicy < ApplicationPolicy
   def index?
     staff?
   end
 
+  alias new? index?
   alias create? index?
+  alias edit? index?
   alias update? index?
+  alias confirm? index?
   alias destroy? index?
 end
