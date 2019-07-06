fe2ba3e67da9461c301d8a33aeca3f4822fe85fc
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-06 07:10:22 +0300

    Remove scope from application policy class

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -5,19 +5,6 @@ class ApplicationPolicy
     @user = user
     @record = record
   end
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      scope.all
-    end
-  end
 end
 
 # class ApplicationPolicy
