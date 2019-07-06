9f492d173fafc042b08ac0b40b7a03318fffd62c
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-06 18:55:49 +0300

    Sub ban policy

---
+++ app/policies/sub_ban_policy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class SubBanPolicy < ApplicationPolicy
+  def index?
+    staff? || sub_moderator?(record)
+  end
+
+  alias search? index?
+  alias new? index?
+  alias create? index?
+  alias edit? index?
+  alias update? index?
+  alias confirm? index?
+  alias destroy? index?
+end
