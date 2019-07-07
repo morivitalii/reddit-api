4d604717325012d1d0308d69af48b36cb6d8a174
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-07 10:11:55 +0300

    Tag thing policy

--- app/policies/tag_thing_policy.rb
+++ app/policies/tag_thing_policy.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class TagThingPolicy < ApplicationPolicy
-  def update?(thing)
-    staff? || sub_moderator?(thing.sub)
+  def update?
+    staff? || sub_moderator?(record.sub)
   end
+
+  alias edit? update?
 end
