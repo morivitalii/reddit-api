e60bf56d0c965e56ce8cb0745a9d4b1a6a8d1535
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-07-25 02:55:42 +0300

    Eliminate js code duplicates which works with entries (#224)

--- app/assets/javascripts/blacklisted_domains.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(document).on('ajax:success', '#blacklisted_domains .create', function(e) {
-        $('#blacklisted_domains').append(e.detail[0].activeElement.innerHTML);
-        $('.modal').modal('show');
-    });
-});
--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    $(document).on('ajax:success', '#bans .create', function(e) {
+    $(document).on('ajax:success', '.entries .create', function(e) {
         $('#bans').append(e.detail[0].activeElement.innerHTML);
         $('.modal').modal('show');
     });
 
-    $(document).on('ajax:success', '#bans .update', function(e) {
+    $(document).on('ajax:success', '.entries .update', function(e) {
         $(this).closest('.entry').append(e.detail[0].activeElement.innerHTML);
         $('.modal').modal('show');
     });
 
-    $(document).on('ajax:success', '#bans .updateForm', function (e) {
+    $(document).on('ajax:success', '.entries .updateForm', function (e) {
         var entry = $(this).closest('.entry');
         $('.modal').modal('hide');
         entry.replaceWith(e.detail[0].activeElement.innerHTML);
         format_datetime();
     });
 
-    $(document).on('click', '#bans .toggleDetails', function() {
+    $(document).on('click', '.entries .toggleDetails', function() {
         $(this).closest('.entry').find('.details').toggleClass('d-none');
     });
 });
