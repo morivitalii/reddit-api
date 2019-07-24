d7fb0a4a6a9bdc7e4fee3390c31478548ec0eaf8
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-25 02:55:14 +0300

    Eliminate js code duplicates which works with entries

--- app/assets/javascripts/logs.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $(document).on('click', '#logs .toggleDetails', function() {
-        $(this).closest('.entry').find('.details').toggleClass('d-none');
-    });
-});
