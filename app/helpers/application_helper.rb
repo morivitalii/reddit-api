dbfaa1ef1e59230ea4ea50360ac87f2019ad6229
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-07-25 03:55:19 +0300

    User profile navigation refactoring (#227)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,26 +12,4 @@ module ApplicationHelper
   def datetime_tag(time, format)
     content_tag("span", "", class: "datetime-#{format}", data: { timestamp: time.to_i })
   end
-
-  def user_profile_menu(user)
-    menu = {
-      t("posts_and_comments") => user_path(user, type: :all),
-      t("posts") => user_path(user, type: :post),
-      t("comments") =>user_path(user, type: :comment)
-    }
-
-    if Current.user&.id == user.id
-      menu[t("notifications")] = notifications_path
-    end
-
-    if BookmarkPolicy.new(pundit_user, nil).index?
-      menu[t("bookmarks")] = bookmarks_path
-    end
-
-    if VotePolicy.new(pundit_user, nil).index?
-      menu[t("votes")] = votes_path
-    end
-
-    menu
-  end
 end
