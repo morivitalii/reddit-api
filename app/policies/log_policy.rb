624b2e8a70ae4ffbc37c4286168f7b5975fc67d5
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-28 09:57:09 +0300

    Remove logs

--- app/policies/log_policy.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class LogPolicy < ApplicationPolicy
-  def index?
-    user_signed_in? && context.user.moderator?(context.sub)
-  end
-end
--- lib/tasks/cleanup/logs.rake
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-namespace :cleanup do
-  desc "Cleanup logs"
-  task logs: :environment do
-    Cleanup::Logs.call
-  end
-end
