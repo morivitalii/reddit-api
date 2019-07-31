54a10d0e9040489acdcc0d0f523b1675ec3cea26
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-07-31 09:40:06 +0300

    Contributor policy refactoring and tests

--- spec/factories/contributor.rb
+++ spec/factories/contributor.rb
@@ -2,8 +2,19 @@
 
 FactoryBot.define do
   factory :contributor do
-    sub
+    global
     user
     association :approved_by, factory: :user
+
+    trait :global do
+      sub { nil }
+    end
+
+    trait :sub do
+      sub
+    end
+
+    factory :global_contributor, traits: [:global]
+    factory :sub_contributor, traits: [:sub]
   end
 end
