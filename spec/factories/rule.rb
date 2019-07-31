e7ae6afce5d99f1b4ff49265bafdc5f2ded2300b
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-07-31 10:16:16 +0300

    Rule policy refactoring and tests (#258)

--- spec/factories/rule.rb
+++ spec/factories/rule.rb
@@ -1,7 +1,18 @@
 FactoryBot.define do
   factory :rule do
-    sub
+    global
     sequence(:title) { |i| "Title #{i}" }
     sequence(:description) { |i| "Description #{i}" }
+
+    trait :global do
+      sub { nil }
+    end
+
+    trait :sub do
+      sub
+    end
+
+    factory :global_rule, traits: [:global]
+    factory :sub_rule, traits: [:sub]
   end
 end
