687a8b88f87638e3c42e6673f0267185cecb5893
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-07-31 16:26:08 +0300

    Skip verify_authorized in page not found controller (#260)

--- app/controllers/page_not_found_controller.rb
+++ app/controllers/page_not_found_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PageNotFoundController < ApplicationController
+  skip_after_action :verify_authorized, only: [:show]
+
   def show
     render "/page_not_found", status: :not_found
   end
