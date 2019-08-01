2cbe75ca1481432c41343db3131f9ff5cf2bb732
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-01 07:12:43 +0300

    Password policy (#266)

---
+++ app/policies/password_policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class PasswordPolicy < ApplicationPolicy
+  def update?
+    true
+  end
+
+  alias edit? update?
+
+  def permitted_attributes_for_update
+    [:token, :password]
+  end
+end
