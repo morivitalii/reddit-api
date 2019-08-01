28e292a1e0dde09f1b1a8edea26064423edc7521
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-01 07:25:57 +0300

    Sign up policy

---
+++ spec/policies/sign_up_policy_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe SignUpPolicy do
+  subject { described_class }
+
+  context "for visitor" do
+    let(:user) { nil }
+    let(:context) { UserContext.new(user) }
+
+    permissions :new?, :create? do
+      it "grants access" do
+        expect(subject).to permit(context)
+      end
+    end
+  end
+
+  context "for user" do
+    let(:user) { create(:user) }
+    let(:context) { UserContext.new(user) }
+
+    permissions :new?, :create? do
+      it "denies access" do
+        expect(subject).to_not permit(context)
+      end
+    end
+  end
+end
