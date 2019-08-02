6aa1d2f0283db392d951936f13de614a283d9c49
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-02 18:34:43 +0300

    Contributors query object (#282)

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -6,8 +6,4 @@ class Contributor < ApplicationRecord
   belongs_to :sub, optional: true
   belongs_to :user
   belongs_to :approved_by, class_name: "User", foreign_key: "approved_by_id"
-
-  def self.search(query)
-    joins(:user).where("lower(users.username) = ?", query)
-  end
 end
---
+++ spec/queries/contributors_query_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe ContributorsQuery do
+  subject { described_class.new }
+
+  describe ".where_sub" do
+    let!(:sub) { create(:sub) }
+    let!(:contributors) { [create(:sub_contributor, sub: sub)] }
+
+    it "returns sub contributors" do
+      expected_result = contributors
+      result = subject.where_sub(sub).all
+
+      expect(result).to eq(expected_result)
+    end
+  end
+
+  describe ".where_global" do
+    let!(:contributors) { [create(:global_contributor)] }
+
+    it "returns global contributors" do
+      expected_result = contributors
+      result = subject.where_global.all
+
+      expect(result).to eq(expected_result)
+    end
+  end
+
+  describe ".filter_by_username" do
+    let!(:contributors) { create_pair(:contributor) }
+
+    it "returns contributors if given user username is blank" do
+      expected_result = contributors
+      result = subject.filter_by_username(nil).all
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "returns contributor where user have a given username" do
+      expected_result = [contributors.first]
+      username = expected_result.first.user.username
+      result = subject.filter_by_username(username).all
+
+      expect(result).to eq(expected_result)
+    end
+  end
+end
