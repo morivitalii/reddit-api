d69bbfcfe6d1788ba2864924e9695d5995a611f3
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-03 09:52:38 +0300

    Dont load seeds in test environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-ActiveRecord::Base.transaction do
-  readmaru = SignUp.new(username: "readmaru", password: "password").save
-  SignUp.new(username: "AutoModerator", password: "password").save
+unless Rails.env.test?
+  ActiveRecord::Base.transaction do
+    readmaru = SignUp.new(username: "readmaru", password: "password").save
+    SignUp.new(username: "AutoModerator", password: "password").save
 
-  Sub.create!(user: readmaru, url: "all", title: "All")
+    Sub.create!(user: readmaru, url: "all", title: "All")
+  end
 end
