c5dbbaeaf733bf8ef0a1988bae2f8c8c5e4c355f
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-06 10:06:36 +0300

    Create blacklisted domain form refactoring and tests (#344)

--- app/forms/create_blacklisted_domain_form.rb
+++ app/forms/create_blacklisted_domain_form.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class CreateBlacklistedDomain
+class CreateBlacklistedDomainForm
   include ActiveModel::Model
 
-  attr_accessor :current_user, :sub, :domain
+  attr_accessor :sub, :domain
   attr_reader :blacklisted_domain
 
   def save
     @blacklisted_domain = BlacklistedDomain.create!(
-      sub: @sub,
-      domain: @domain
+      sub: sub,
+      domain: domain
     )
   rescue ActiveRecord::RecordInvalid => invalid
     errors.merge!(invalid.record.errors)
