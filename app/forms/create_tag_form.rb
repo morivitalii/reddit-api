c3f504c18c40a109e493cb08db81e772749ef577
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-06 10:54:51 +0300

    Create tag form refactoring and tests (#351)

--- app/forms/create_tag_form.rb
+++ app/forms/create_tag_form.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class CreateTag
+class CreateTagForm
   include ActiveModel::Model
 
-  attr_accessor :sub, :current_user, :title
+  attr_accessor :sub, :title
   attr_reader :tag
 
   def save
     @tag = Tag.create!(
-      sub: @sub,
-      title: @title
+      sub: sub,
+      title: title
     )
   rescue ActiveRecord::RecordInvalid => invalid
     errors.merge!(invalid.record.errors)
