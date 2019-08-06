c6f9f4a6f67a48255065e3c6e948b7ede63ca192
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-06 11:38:25 +0300

    Update ban form refactoring and tests (#355)

---
+++ app/forms/update_ban_form.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class UpdateBanForm
+  include ActiveModel::Model
+
+  attr_accessor :ban, :reason, :days, :permanent
+
+  def save
+    ban.update!(
+      reason: reason,
+      days: days,
+      permanent: permanent
+    )
+  rescue ActiveRecord::RecordInvalid => invalid
+    errors.merge!(invalid.record.errors)
+
+    return false
+  end
+end
