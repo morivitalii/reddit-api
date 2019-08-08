747526402467da10a061ca0e7b449da50939b14c
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-08 11:15:04 +0300

    User not contributor validator refactoring and tests (#370)

--- app/validators/user_not_contributor_validator.rb
+++ app/validators/user_not_contributor_validator.rb
@@ -4,15 +4,21 @@ class UserNotContributorValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    if scope(record.sub, value).exists?
+    if scope(record, value).exists?
       record.errors.add(attribute, :user_contributor)
     end
   end
 
   private
 
-  def scope(sub, username)
+  def scope(record, username)
+    sub = sub(record)
+
     scope = ContributorsQuery.new.global_or_sub(sub)
     ContributorsQuery.new(scope).filter_by_username(username)
+  end
+
+  def sub(record)
+    record.respond_to?(:sub) ? record.sub : nil
   end
 end
