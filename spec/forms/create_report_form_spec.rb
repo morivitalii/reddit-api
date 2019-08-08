0ab19dc15da6dcd0f6bf8bb143a53f173436793b
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-08 09:44:50 +0300

    Create report form refactoring and tests

---
+++ spec/forms/create_report_form_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe CreateReportForm do
+  subject { described_class }
+
+  let(:user) { create(:user) }
+  let(:reportable) { create(:post) }
+  let(:text) { "Text" }
+
+  describe ".save" do
+    before do
+      @form = subject.new(
+        reportable: reportable,
+        user: user,
+        text: text
+      )
+    end
+
+    it "returns true if reportable.ignore_reports? is true" do
+      allow(@form).to receive(:skip?).and_return(true)
+
+      expected_result = true
+      result = @form.save
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "does not create report if one from user to reportable already exists" do
+      create(:report, reportable: reportable, user: user)
+
+      expect { @form.save }.to_not change { Report.count }
+    end
+
+    it "returns report if already exists" do
+      expected_result = create(:report, reportable: reportable, user: user)
+      result = @form.save
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "creates report" do
+      expected_attributes = { reportable: reportable, user: user, sub: reportable.sub, text: text }
+      result = @form.save
+
+      expect(result).to have_attributes(expected_attributes)
+    end
+  end
+end
