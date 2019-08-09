f6aa443c0ce10deae7028a484bc8f23659b9dfea
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-09 12:21:58 +0300

    Blacklisted domain refactoring and tests (#384)

--- spec/models/ban_spec.rb
+++ spec/models/ban_spec.rb
@@ -1,58 +1,52 @@
 require "rails_helper"
 
 RSpec.describe Ban do
-  subject { build(described_class.to_s.underscore.to_sym) }
+  subject { described_class }
 
   it_behaves_like "paginatable"
   it_behaves_like "squished attribute", :reason
 
   it "set end_at attribute before save to nil if permanent" do
-    subject.permanent = true
-    subject.days = nil
-    subject.save!
+    model = build(:ban, permanent: true, days: nil)
+    model.save!
 
-    expect(subject.end_at).to be_nil
+    expect(model.end_at).to be_nil
   end
 
   it "set end_at attribute before save to calculated datetime if temporary" do
-    subject.permanent = false
-    subject.days = 1
-    subject.save!
+    model = build(:ban, permanent: false, days: 1)
+    model.save!
 
-    result = subject.end_at
-    expected_result = subject.created_at + 1.day
+    result = model.end_at
+    expected_result = model.created_at + 1.day
 
     expect(result).to eq(expected_result)
   end
 
   describe ".stale?" do
     context "permanent" do
-      before do
-        subject.permanent = true
-      end
-
       it "returns false" do
-        result = subject.stale?
+        model = build(:ban, permanent: true, days: nil)
+        model.save!
+        result = model.stale?
 
         expect(result).to be_falsey
       end
     end
 
     context "temporary" do
-      before do
-        subject.permanent = false
-      end
-
       it "returns true if end_at before current datetime" do
-        subject.end_at = Time.current - 1.day
-        result = subject.stale?
+        model = build(:ban, created_at: 2.days.ago, permanent: false, days: 1)
+        model.save!
+        result = model.stale?
 
         expect(result).to be_truthy
       end
 
       it "returns false if end_at after current datetime" do
-        subject.end_at = Time.current + 1.day
-        result = subject.stale?
+        model = build(:ban, created_at: Time.current, permanent: false, days: 1)
+        model.save!
+        result = model.stale?
 
         expect(result).to be_falsey
       end
--- spec/models/blacklisted_domain_spec.rb
+++ spec/models/blacklisted_domain_spec.rb
@@ -1,5 +1,31 @@
 require "rails_helper"
 
 RSpec.describe BlacklistedDomain do
+  subject { described_class }
+
   it_behaves_like "paginatable"
+
+  describe "uniqueness validation" do
+    context "not unique" do
+      let(:blacklisted_domain) { create(:blacklisted_domain) }
+
+      it "adds error on domain attribute" do
+        model = subject.new(domain: blacklisted_domain.domain)
+        model.validate
+
+        expected_result = { error: :taken }
+        result = model.errors.details[:domain]
+
+        expect(result).to include(expected_result)
+      end
+    end
+
+    context "unique" do
+      it "is valid" do
+        model = subject.new(domain: "example.com")
+
+        expect(model.valid?).to be_truthy
+      end
+    end
+  end
 end
