38622b431328f0e0afd2d29fb0ee7d2cb7964d56
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-09 12:05:32 +0300

    Ban model refactoring and tests

--- app/models/ban.rb
+++ app/models/ban.rb
@@ -7,34 +7,26 @@ class Ban < ApplicationRecord
   belongs_to :user
   belongs_to :banned_by, class_name: "User", foreign_key: "banned_by_id"
 
-  before_create :delete_user_as_contributor_on_create
   before_save :set_end_at_on_save
 
   validates :reason, allow_blank: true, length: { maximum: 500 }
   validates :days, absence: true, if: ->(record) { record.permanent }
-  validates :days, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 365 }, if: ->(record) { !record.permanent }
+  validates :days, presence: true,
+            numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 365 },
+            if: ->(record) { !record.permanent }
 
   def reason=(value)
     super(value&.squish)
   end
 
   def stale?
-    return false if permanent?
-
-    end_at < Time.current
+    permanent? ? false : end_at < Time.current
   end
 
   private
 
-  def delete_user_as_contributor_on_create
-    user.contributors.where(sub: sub).destroy_all
-  end
-
   def set_end_at_on_save
-    if permanent?
-      self.end_at = nil
-    else
-      self.end_at = (created_at.presence || Time.current) + days.days
-    end
+    self.created_at ||= Time.current
+    self.end_at = permanent? ? nil : created_at + days.days
   end
 end
--- spec/models/ban_spec.rb
+++ spec/models/ban_spec.rb
@@ -1,5 +1,61 @@
 require "rails_helper"
 
 RSpec.describe Ban do
+  subject { build(described_class.to_s.underscore.to_sym) }
+
   it_behaves_like "paginatable"
+  it_behaves_like "squished attribute", :reason
+
+  it "set end_at attribute before save to nil if permanent" do
+    subject.permanent = true
+    subject.days = nil
+    subject.save!
+
+    expect(subject.end_at).to be_nil
+  end
+
+  it "set end_at attribute before save to calculated datetime if temporary" do
+    subject.permanent = false
+    subject.days = 1
+    subject.save!
+
+    result = subject.end_at
+    expected_result = subject.created_at + 1.day
+
+    expect(result).to eq(expected_result)
+  end
+
+  describe ".stale?" do
+    context "permanent" do
+      before do
+        subject.permanent = true
+      end
+
+      it "returns false" do
+        result = subject.stale?
+
+        expect(result).to be_falsey
+      end
+    end
+
+    context "temporary" do
+      before do
+        subject.permanent = false
+      end
+
+      it "returns true if end_at before current datetime" do
+        subject.end_at = Time.current - 1.day
+        result = subject.stale?
+
+        expect(result).to be_truthy
+      end
+
+      it "returns false if end_at after current datetime" do
+        subject.end_at = Time.current + 1.day
+        result = subject.stale?
+
+        expect(result).to be_falsey
+      end
+    end
+  end
 end
