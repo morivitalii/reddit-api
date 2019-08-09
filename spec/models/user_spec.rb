d2faf2d0058ed8733e54a7450fff878f815bc6b1
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-09 17:57:48 +0300

    User model tests (#393)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,15 @@ require "rails_helper"
 
 RSpec.describe User do
   subject { described_class }
+
+  describe ".to_param" do
+    it "returns username" do
+      username = "username"
+      model = subject.new(username: username)
+      expected_result = username
+      result = model.to_param
+
+      expect(result).to eq(expected_result)
+    end
+  end
 end
