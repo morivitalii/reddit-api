a37c92a81b392405b85802edc027718288a4af61
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-09 12:51:01 +0300

    Moderator model refactoring

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -6,12 +6,4 @@ class Moderator < ApplicationRecord
   belongs_to :sub, optional: true
   belongs_to :user
   belongs_to :invited_by, class_name: "User", foreign_key: "invited_by_id"
-
-  before_create :delete_user_as_contributor_on_create, if: -> (record) { record.sub.present? }
-
-  private
-
-  def delete_user_as_contributor_on_create
-    user.contributors.where(sub: sub).destroy_all
-  end
 end
