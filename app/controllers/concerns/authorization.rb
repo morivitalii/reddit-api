5eacc5149340da43c5258784f5b1508d4ff4426f
Vitaly Igorevich <dev1vitaly@users.noreply.github.com>
2019-08-10 10:26:30 +0300

    Remove Current class (#394)

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -4,18 +4,10 @@ module Authorization
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_current_user
-
-    private
-
-    def set_current_user
-      Current.user = request.env["warden"].user
-    end
-
     helper_method :current_user
 
     def current_user
-      Current.user
+      request.env["warden"].user
     end
 
     def pundit_user
