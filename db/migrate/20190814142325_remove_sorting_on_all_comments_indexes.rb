bed8b01b2eba45516ae586967bf5c87fbcdfbbbc
Vitaly <dev1vitaly@users.noreply.github.com>
2019-08-14 17:28:45 +0300

    Remove predefined sorting on all indexes (#413)

---
+++ db/migrate/20190814142325_remove_sorting_on_all_comments_indexes.rb
@@ -0,0 +1,9 @@
+class RemoveSortingOnAllCommentsIndexes < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :comments, column: :hot_score, order: { hot_score: :desc }
+    add_index :comments, :hot_score
+
+    remove_index :comments, column: :new_score, order: { new_score: :desc }
+    add_index :comments, :new_score
+  end
+end
