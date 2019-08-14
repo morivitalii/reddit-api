1dbe933319cf7df3ec003ec0165d3dbc78e1ba48
Vitaly <dev1vitaly@users.noreply.github.com>
2019-08-14 16:48:33 +0300

    Remove pages (#407)

--- spec/factories/page.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :page do
-    sub
-    association :edited_by, factory: :user
-
-    sequence(:title) { |i| "Title #{i}" }
-    sequence(:text) { |i| "Text #{i}" }
-  end
-end
