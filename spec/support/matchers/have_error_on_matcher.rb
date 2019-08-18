dc3cecb8ca9fab9def5f2598de3e480a953782cd
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-18 21:24:39 +0300

    BansController routing spec

--- spec/support/matchers/have_error_on_matcher.rb
+++ spec/support/matchers/have_error_on_matcher.rb
@@ -1,20 +1,20 @@
 require "rspec/expectations"
 
-RSpec::Matchers.define :have_error do |value|
-  match do |record|
-    @actual = record.errors.details[attribute]
+RSpec::Matchers.define :have_error do |expected|
+  match do |actual|
+    @actual = actual.errors.details[attribute]
 
-    @actual.any? { |error| error.value?(value) }
+    @actual.any? { |error| error.value?(expected) }
   end
 
   chain :on, :attribute
   diffable
 
   failure_message do |_|
-    "expected to have error #{value} on #{attribute} attribute"
+    "expected to have error #{expected} on #{attribute} attribute"
   end
 
   failure_message_when_negated do |_|
-    "expected to not have error #{value} on #{attribute} attribute"
+    "expected to not have error #{expected} on #{attribute} attribute"
   end
 end
