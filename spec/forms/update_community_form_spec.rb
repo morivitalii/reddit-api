88722b8955575bae5bfa52ffe9e3b18957c07e17
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-18 19:58:08 +0300

    UpdateCommunityForm spec refactoring

--- spec/forms/update_community_form_spec.rb
+++ spec/forms/update_community_form_spec.rb
@@ -4,16 +4,24 @@ RSpec.describe UpdateCommunityForm, type: :form do
   subject { described_class }
 
   describe ".save" do
-    let(:community) { instance_double(Community, update!: "") }
+    it "updates community" do
+      form = build_update_community_form
 
-    before do
-      @form = subject.new(community: community)
+      form.save
+
+      community = form.community
+      expect(community.title).to eq(form.title)
+      expect(community.description).to eq(form.description)
     end
+  end
 
-    it "calls .update! on community" do
-      @form.save
+  def build_update_community_form
+    community = create(:community)
 
-      expect(community).to have_received(:update!).with(any_args).once
-    end
+    described_class.new(
+      community: community,
+      title: "Title",
+      description: "Description"
+    )
   end
 end
