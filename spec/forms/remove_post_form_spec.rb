1f83fa1d4a4ee882da90e6b83cdf10947088def1
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-18 19:34:24 +0300

    RemovePostForm spec refactoring

--- spec/forms/remove_post_form_spec.rb
+++ spec/forms/remove_post_form_spec.rb
@@ -4,22 +4,26 @@ RSpec.describe RemovePostForm, type: :form do
   subject { described_class }
 
   describe ".save" do
-    let(:user) { instance_double(User) }
-    let(:post) { instance_double(Post) }
-    let(:reason) { "Reason" }
+    it "removes post" do
+      form = build_remove_post_form
+      form.save
 
-    before do
-      @form = subject.new(
-        post: post,
-        user: user,
-        reason: reason
-      )
+      post = form.post
+
+      expect(post.removed_by).to eq(form.user)
+      expect(post.removed_at).to be_present
+      expect(post.removed_reason).to eq(form.reason)
     end
+  end
 
-    it "call .remove! on post with args" do
-      expect(post).to receive(:remove!).with(user, reason).once
+  def build_remove_post_form
+    post = create(:post)
+    user = create(:user)
 
-      @form.save
-    end
+    described_class.new(
+      post: post,
+      user: user,
+      reason: "Reason"
+    )
   end
 end
