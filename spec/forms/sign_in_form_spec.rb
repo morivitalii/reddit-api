4147968748fed91979ed5ccb746ce5272617d1fa
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-18 19:45:49 +0300

    SignInForm spec refactoring

--- spec/forms/sign_in_form_spec.rb
+++ spec/forms/sign_in_form_spec.rb
@@ -3,29 +3,58 @@ require "rails_helper"
 RSpec.describe SignInForm, type: :form do
   subject { described_class }
 
-  describe "validations" do
-    context "invalid" do
-      before do
-        @form = subject.new(username: "username", password: "password")
+  context "when user with given username does not exist" do
+    it "is invalid" do
+      form = build_sign_in_form
+      form.username = "wrong_username"
+
+      expect(form).to_not be_valid
+    end
+
+    it "has error :invalid_credentials on username attribute" do
+      form = build_sign_in_form
+      form.password = "wrong_password"
+
+      form.validate
+
+      expect(form).to have_error(:invalid_credentials).on(:username)
+    end
+  end
+
+  context "when user with given username exists" do
+    context "and password does not match" do
+      it "is invalid" do
+        form = build_sign_in_form
+        form.password = "wrong_password"
+
+        expect(form).to_not be_valid
       end
 
-      it "is invalid if credentials does not match" do
-        @form.validate
+      it "has error :invalid_credentials on username attribute" do
+        form = build_sign_in_form
+        form.password = "wrong_password"
 
-        expect(@form).to have_error(:invalid_credentials).on(:username)
+        form.validate
+
+        expect(form).to have_error(:invalid_credentials).on(:username)
       end
     end
 
-    context "valid" do
-      before do
-        @user = instance_double(User)
-        allow(@user).to receive(:authenticate).with(anything).and_return(true)
+    context "and credentials match" do
+      it "is valid" do
+        form = build_sign_in_form
 
-        @form = subject.new(username: double, password: double)
-        allow(@form).to receive(:user).and_return(@user)
+        expect(form).to be_valid
       end
-
-      it { expect(@form).to be_valid }
     end
+  end
+
+  def build_sign_in_form
+    user = create(:user)
+
+    described_class.new(
+      username: user.username,
+      password: user.password
+    )
   end
 end
