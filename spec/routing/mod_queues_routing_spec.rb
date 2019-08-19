9e946fbbce77d2e2b9c9d9b385c446827bb6deed
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-19 14:32:10 +0300

    ModQueues routing spec

---
+++ spec/routing/mod_queues_routing_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe "routes for mod_queuesController", type: :routing do
+  it "routes GET /communities/all/mod_queues/new_posts to mod_queues#new_posts" do
+    expect(get("/communities/all/mod_queues/new_posts")).to route_to(controller: "mod_queues", action: "new_posts", community_id: "all")
+  end
+
+  it "routes GET /communities/all/mod_queues/new_comments to mod_queues#new_comments" do
+    expect(get("/communities/all/mod_queues/new_comments")).to route_to(controller: "mod_queues", action: "new_comments", community_id: "all")
+  end
+
+  it "routes GET /communities/all/mod_queues/reported_posts to mod_queues#reported_posts" do
+    expect(get("/communities/all/mod_queues/reported_posts")).to route_to(controller: "mod_queues", action: "reported_posts", community_id: "all")
+  end
+
+  it "routes GET /communities/all/mod_queues/reported_comments to mod_queues#reported_comments" do
+    expect(get("/communities/all/mod_queues/reported_comments")).to route_to(controller: "mod_queues", action: "reported_comments", community_id: "all")
+  end
+
+  it "routes GET /communities/all/mod_queues to page_not_found#show" do
+    expect(get("/communities/all/mod_queues")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues")
+  end
+
+  it "routes GET /communities/all/mod_queues/1 to page_not_found#show" do
+    expect(get("/communities/all/mod_queues/1")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/1")
+  end
+
+  it "routes GET /communities/all/mod_queues/new to page_not_found#show" do
+    expect(get("/communities/all/mod_queues/new")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/new")
+  end
+
+  it "routes POST /communities/all/mod_queues to page_not_found#show" do
+    expect(post("/communities/all/mod_queues")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues")
+  end
+
+  it "routes GET /communities/all/mod_queues/1/edit to page_not_found#show" do
+    expect(get("/communities/all/mod_queues/1/edit")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/1/edit")
+  end
+
+  it "routes PUT /communities/all/mod_queues/1 to page_not_found#show" do
+    expect(put("/communities/all/mod_queues/1")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/1")
+  end
+
+  it "routes PATCH /communities/all/mod_queues/1 to page_not_found#show" do
+    expect(patch("/communities/all/mod_queues/1")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/1")
+  end
+
+  it "routes DELETE /communities/all/mod_queues/1 to page_not_found#show" do
+    expect(delete("/communities/all/mod_queues/1")).to route_to(controller: "page_not_found", action: "show", path: "communities/all/mod_queues/1")
+  end
+end
