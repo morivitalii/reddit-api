894ea22fc64b785bdfecee50d4536d1d0075ae0b
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-20 23:02:03 +0300

    Report spec refactoring

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Report, type: :model do
   subject { described_class }
 
   it_behaves_like "paginatable"
+
+  describe "validations" do
+    subject { build(:report) }
+
+    it { is_expected.to validate_presence_of(:text) }
+    it { is_expected.to validate_length_of(:text).is_at_most(500) }
+  end
 end
