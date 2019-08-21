08a11dc7d744676345940b57d28330def8e54fe6
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-22 01:17:45 +0300

    Remove RemovableDecorator concern

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class CommentDecorator < ApplicationDecorator
-  include RemovableDecorator
   include ReportableDecorator
   include VotableDecorator
   include CommentableDecorator
@@ -50,5 +49,39 @@ class CommentDecorator < ApplicationDecorator
     bookmarked = model.bookmark.present?
 
     bookmarked ? h.t('delete_bookmark') : h.t('bookmark')
+  end
+
+  def remove_link
+    removed = model.removed?
+
+    link_icon = h.fa_icon("trash")
+    link_path = [:remove, model]
+    link_class = removed ? "remove text-danger" : "remove"
+
+    h.link_to(link_icon, link_path, remote: true, class: link_class, data: { toggle: :tooltip }, title: remove_link_tooltip_message)
+  end
+
+  def remove_link_tooltip_message
+    removed = model.removed?
+
+    if removed
+      username = model.removed_by.username
+      reason = model.removed_reason
+      removed_at = h.l(model.removed_at)
+
+      h.t("deletion_details", username: username, removed_at: removed_at, reason: reason)
+    else
+      h.t("delete")
+    end
+  end
+
+  def removed_message
+    removed_by = model.removed_by
+    removed_at = h.datetime_ago_tag(model.removed_at)
+    reason = model.removed_reason
+
+    link_to_user_profile = h.link_to(removed_by.username, h.posts_user_path(removed_by))
+
+    h.t("removed_html", username: link_to_user_profile, removed_at: removed_at, reason: reason)
   end
 end
