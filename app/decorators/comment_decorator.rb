b2138b4a22b45b3b97396ca9a5ae1ed575cc25ee
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-22 01:18:53 +0300

    Remove VotableDecorator concern

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,13 +2,42 @@
 
 class CommentDecorator < ApplicationDecorator
   include ReportableDecorator
-  include VotableDecorator
   include CommentableDecorator
 
   def edited_at
     edited_at = h.datetime_ago_tag(model.edited_at)
 
     h.t('edited_html', edited_at: edited_at)
+  end
+
+  def up_vote_link
+    up_voted = model.vote&.up?
+
+    link_class = up_voted ? "up text-success mr-1p" : "up mr-1"
+    link_icon = h.fa_icon("arrow-up")
+    link_path = [model, :votes]
+    link_data_params = up_voted ? "" : "create_vote_form[type]=up"
+    link_method = up_voted ? :delete : :post
+
+    h.link_to(link_icon, link_path, data: { params: link_data_params }, remote: true, method: link_method, class: link_class)
+  end
+
+  def score
+    score = h.number_to_human(model.score, separator: ".", strip_insignificant_zeros: true, units: { thousand: "k" }, format: "%n%u")
+
+    h.content_tag(:span, score, class: "score")
+  end
+
+  def down_vote_link
+    down_voted = model.vote&.down?
+
+    link_class = down_voted ? "down text-danger mr-1" : "down mr-1"
+    link_icon = h.fa_icon("arrow-down")
+    link_path = [model, :votes]
+    link_data_params = down_voted ? "" : "create_vote_form[type]=down"
+    link_method = down_voted ? :delete : :post
+
+    h.link_to(link_icon, link_path, data: { params: link_data_params }, remote: true, method: link_method, class: link_class)
   end
 
   def approve_link
--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,7 +2,6 @@
 
 class PostDecorator < ApplicationDecorator
   include ReportableDecorator
-  include VotableDecorator
   include CommentableDecorator
 
   def created_at
@@ -13,6 +12,36 @@ class PostDecorator < ApplicationDecorator
     edited_at = h.datetime_ago_tag(model.edited_at)
 
     h.t('edited_html', edited_at: edited_at)
+  end
+
+  def up_vote_link
+    up_voted = model.vote&.up?
+
+    link_class = up_voted ? "up text-success mr-1p" : "up mr-1"
+    link_icon = h.fa_icon("arrow-up")
+    link_path = [model, :votes]
+    link_data_params = up_voted ? "" : "create_vote_form[type]=up"
+    link_method = up_voted ? :delete : :post
+
+    h.link_to(link_icon, link_path, data: { params: link_data_params }, remote: true, method: link_method, class: link_class)
+  end
+
+  def score
+    score = h.number_to_human(model.score, separator: ".", strip_insignificant_zeros: true, units: { thousand: "k" }, format: "%n%u")
+
+    h.content_tag(:span, score, class: "score")
+  end
+
+  def down_vote_link
+    down_voted = model.vote&.down?
+
+    link_class = down_voted ? "down text-danger mr-1" : "down mr-1"
+    link_icon = h.fa_icon("arrow-down")
+    link_path = [model, :votes]
+    link_data_params = down_voted ? "" : "create_vote_form[type]=down"
+    link_method = down_voted ? :delete : :post
+
+    h.link_to(link_icon, link_path, data: { params: link_data_params }, remote: true, method: link_method, class: link_class)
   end
 
   def content_processing?
