2a42f2502ff06d40b02bcc38d7b84b885d6cc22f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-22 01:20:39 +0300

    Remove CommentableDecorator concern

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class CommentDecorator < ApplicationDecorator
-  include CommentableDecorator
+  def comments_link
+    comments_count = model.comments_count
+    comments_pluralized = h.t('comments_count', count: comments_count)
+    link_title = "#{h.number_to_human(comments_count, separator: '.', strip_insignificant_zeros: true, units: { thousand: 'k' })} #{comments_pluralized}"
+    link_path = [model]
+    link_class = "comments_count"
+
+    h.link_to(link_title, link_path, class: link_class)
+  end
 
   def edited_at
     edited_at = h.datetime_ago_tag(model.edited_at)
