5bf441c93db5e5746dd08a74a7a5008646925181
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-22 21:05:47 +0300

    BanDecorator.username

--- app/decorators/ban_decorator.rb
+++ app/decorators/ban_decorator.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class BanDecorator < ApplicationDecorator
+  def username
+    model.user.username
+  end
+
   def details
     created_at = h.t("ban_created_at_html", created_at: h.datetime_short_tag(model.created_at))
     type = model.permanent? ? h.t("banned_permanently") : h.t("ban_end_at_html", end_at: h.datetime_short_tag(model.end_at))
