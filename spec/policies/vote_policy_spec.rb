0e6a2438b866efc179f7c40e0e31ed890e8c90bf
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-23 17:26:20 +0300

    Specs for VotePolicy permitted attributes

--- spec/policies/vote_policy_spec.rb
+++ spec/policies/vote_policy_spec.rb
@@ -32,4 +32,15 @@ RSpec.describe VotePolicy, type: :policy do
       it { is_expected.to permit(context) }
     end
   end
+
+  describe ".permitted_attributes_for_create" do
+    it "contains attributes" do
+      policy = build_policy
+      expect(policy.permitted_attributes_for_create).to contain_exactly(:type)
+    end
+  end
+
+  def build_policy
+    described_class.new(Context.new(nil), nil)
+  end
 end
