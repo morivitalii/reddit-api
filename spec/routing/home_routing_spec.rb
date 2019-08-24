99a382f6452fe9267ba71310d603ed8732e9bb78
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-24 18:47:21 +0300

    Remove routing specs in favour of system tests

--- spec/routing/home_routing_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "routes for HomeController", type: :routing do
-  it "routes GET / to home#index" do
-    expect(get("/")).to route_to("home#index")
-  end
-end
--- spec/routing/page_not_found_routing_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "routing for PageNotFoundController", type: :routing do
-  it "routes GET /404 to page_not_found#show" do
-    expect(get("/404")).to route_to(controller: "page_not_found", action: "show", path: "404")
-  end
-end
