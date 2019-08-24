373ea83fd631b21160c805353d78b8b2e63fe624
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-24 17:48:11 +0300

    Remove env checkup from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
-unless Rails.env.test?
-  ActiveRecord::Base.transaction do
-    SignUpForm.new(username: "readmaru", password: "password").save
-    SignUpForm.new(username: "AutoModerator", password: "password").save
+ActiveRecord::Base.transaction do
+  SignUpForm.new(username: "readmaru", password: "password").save
+  SignUpForm.new(username: "AutoModerator", password: "password").save
 
-    Community.create!(url: "all", title: "All")
-  end
+  Community.create!(url: "all", title: "All")
 end
