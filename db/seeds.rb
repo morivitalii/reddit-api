e840e3eac373ec953057f2cfa6b97090b5838970
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-27 18:00:56 +0300

    Remove AutoModerator user creation from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 ActiveRecord::Base.transaction do
   SignUpForm.new(username: "readmaru", password: "password").save
-  SignUpForm.new(username: "AutoModerator", password: "password").save
 
   Community.create!(url: "all", title: "All")
 end
