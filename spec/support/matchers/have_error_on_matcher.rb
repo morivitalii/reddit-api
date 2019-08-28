bd168ea9f3f56b909d3dfd2bc4716d8f26912e8d
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-08-28 18:47:42 +0300

    Have error on matcher refactoring

--- spec/support/matchers/have_error_on_matcher.rb
+++ spec/support/matchers/have_error_on_matcher.rb
@@ -1,20 +1,20 @@
 require "rspec/expectations"
 
-RSpec::Matchers.define :have_error do |expected|
-  match do |actual|
-    @actual = actual.errors.details[attribute]
+RSpec::Matchers.define :have_error do |error|
+  match do |model|
+    @actual = model.errors.details[attribute]
 
-    @actual.any? { |error| error.value?(expected) }
+    @actual.any? { |actual_error| actual_error.value?(error) }
   end
 
   chain :on, :attribute
   diffable
 
   failure_message do |_|
-    "expected to have error #{expected} on #{attribute} attribute"
+    "error to have error #{error} on #{attribute} attribute"
   end
 
   failure_message_when_negated do |_|
-    "expected to not have error #{expected} on #{attribute} attribute"
+    "error to not have error #{error} on #{attribute} attribute"
   end
 end
