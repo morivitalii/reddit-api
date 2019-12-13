dd3d1c7169adb542e24fe5382cae2e85fa6635b4
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-14 00:07:26 +0200

    Remove unused attributes from Communities::Posts::UpdateForm

--- app/forms/communities/posts/update_form.rb
+++ app/forms/communities/posts/update_form.rb
@@ -1,30 +1,15 @@
 class Communities::Posts::UpdateForm
   include ActiveModel::Model
 
-  attr_accessor :post, :user, :text, :explicit, :spoiler, :ignore_reports
+  attr_accessor :post, :user, :text
 
   def save
-    attributes = {
-      text: text,
-      explicit: explicit,
-      spoiler: spoiler,
-      ignore_reports: ignore_reports,
-    }.compact
-
-    if edited?
-      post.edit(user)
-    end
-
-    @post.update!(attributes)
+    post.text = text
+    post.edit(user)
+    post.update!(text: text)
   rescue ActiveRecord::RecordInvalid => invalid
     errors.merge!(invalid.record.errors)
 
     false
-  end
-
-  private
-
-  def edited?
-    text.present? && post.text != text
   end
 end
--- spec/forms/communities/posts/update_form_spec.rb
+++ spec/forms/communities/posts/update_form_spec.rb
@@ -8,31 +8,12 @@ RSpec.describe Communities::Posts::UpdateForm do
 
     post = form.post
     expect(post.text).to eq(form.text)
-    expect(post.explicit).to eq(form.explicit)
-    expect(post.spoiler).to eq(form.spoiler)
-    expect(post.ignore_reports).to eq(form.ignore_reports)
-  end
-
-  it "remove attributes with nil values before update" do
-    form = build_update_post_form
-    form.text = nil
-    form.explicit = nil
-    form.spoiler = nil
-    form.ignore_reports = nil
-
-    form.save
-
-    post = form.post
-    expect(post.text).to_not be_blank
-    expect(post.explicit).to_not be_nil
-    expect(post.spoiler).to_not be_nil
-    expect(post.ignore_reports).to_not be_nil
   end
 
   def build_update_post_form
     post = create(:post)
     user = create(:user)
 
-    described_class.new(post: post, user: user, text: "Text", explicit: true, spoiler: true, ignore_reports: true)
+    described_class.new(post: post, user: user, text: "Text")
   end
 end
