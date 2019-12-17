d3a886f8dcec565c04e1182526cb4de0d34c1c51
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-17 02:48:56 +0200

    Disable recaptcha in development mode

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,9 +1,5 @@
 Recaptcha.configure do |config|
-  if Rails.env.production?
-    config.site_key = ENV.fetch("RECAPTCHA_SITE_KEY")
-    config.secret_key = ENV.fetch("RECAPTCHA_SECRET_KEY")
-  elsif Rails.env.development?
-    config.site_key = "6LfSh4oUAAAAAGWI8uuBh4ndFuYUKETljRjCMOx6"
-    config.secret_key = "6LfSh4oUAAAAAMLUb6MBWRyh2J9ZwgGg_xmrz9FI"
-  end
+  config.site_key = ENV.fetch("RECAPTCHA_SITE_KEY")
+  config.secret_key = ENV.fetch("RECAPTCHA_SECRET_KEY")
+  config.skip_verify_env << "development"
 end
