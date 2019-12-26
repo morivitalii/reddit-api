435d3f630b9ef95399119207f31461ba5ff7b2cd
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-27 00:27:47 +0200

    Include Warden::Test::Helpers only in request specs

--- spec/support/warden_helper.rb
+++ spec/support/warden_helper.rb
@@ -1,8 +1,8 @@
 RSpec.configure do |config|
-  config.include Warden::Test::Helpers, type: :system
+  config.include Warden::Test::Helpers, type: :request
 
   # Reset warden after each system test
-  config.after(:each, type: :system) do
+  config.after(:each, type: :request) do
     Warden.test_reset!
   end
 end
