98084f2a4cb017764982b04be5c0218988aa2ea7
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-28 10:32:39 +0200

    Standardrb

--- spec/requests/api/sign_in_spec.rb
+++ spec/requests/api/sign_in_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::SignInController do
       it "signs in user and returns user object" do
         user = create(:user)
 
-        post "/api/sign_in.json", params: { username: user.username, password: user.password }
+        post "/api/sign_in.json", params: {username: user.username, password: user.password}
 
         expect(response).to have_http_status(200)
         expect(session["warden.user.default.key"]).to eq(user.id)
@@ -16,7 +16,7 @@ RSpec.describe Api::SignInController do
 
     context "with wrong credentials" do
       it "does not sign in user and returns error messages" do
-        post "/api/sign_in.json", params: { username: "username", password: "password" }
+        post "/api/sign_in.json", params: {username: "username", password: "password"}
 
         expect(response).to have_http_status(422)
         expect(session["warden.user.default.key"]).to be_nil
