1512d14c8dc824d38c2df4e995257124ca728242
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-28 11:03:38 +0200

    Remove ApplicationController.sidebar_rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  helper_method :sidebar_rules
-  def sidebar_rules
-    @sidebar_rules ||= pundit_user.community.rules.order(id: :asc).all
-  end
-
   helper_method :sidebar_moderators
   def sidebar_moderators
     @sidebar_moderators ||= ModeratorsQuery.new(pundit_user.community.moderators).recent(10).includes(:user)
