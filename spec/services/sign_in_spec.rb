7938bd0da5339953cfd49ca61364e917e5a6fcbd
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-28 11:53:22 +0200

    Update SignIn service

--- spec/services/sign_in_spec.rb
+++ spec/services/sign_in_spec.rb
@@ -1,33 +1,37 @@
 require "rails_helper"
 
 RSpec.describe SignIn do
-  context "when user with given username does not exist" do
-    it "has error :invalid_credentials on username attribute" do
-      form = described_class.new(username: "invalid", password: "password")
-      form.validate
+  describe "validations" do
+    context "when user with given username does not exist" do
+      it "is invalid" do
+        service = described_class.new(username: "invalid", password: "password")
+        service.validate
 
-      expect(form).to have_error(:invalid_credentials).on(:username)
-    end
-  end
-
-  context "when user with given username exists" do
-    context "and password does not match" do
-      it "has error :invalid_credentials on username attribute" do
-        create(:user, username: "username", password: "password")
-        form = described_class.new(username: "username", password: "wrong_password")
-
-        form.validate
-
-        expect(form).to have_error(:invalid_credentials).on(:username)
+        expect(service).to_not be_valid
+        expect(service).to have_error(:invalid_credentials).on(:username)
       end
     end
 
-    context "and credentials match" do
-      it "is valid" do
-        create(:user, username: "username", password: "password")
-        form = described_class.new(username: "username", password: "password")
+    context "when user with given username exists" do
+      context "and password does not match" do
+        it "is invalid" do
+          create(:user, username: "username", password: "password")
+          service = described_class.new(username: "username", password: "wrong_password")
 
-        expect(form).to be_valid
+          service.validate
+
+          expect(service).to_not be_valid
+          expect(service).to have_error(:invalid_credentials).on(:username)
+        end
+      end
+
+      context "and password match" do
+        it "is valid" do
+          create(:user, username: "username", password: "password")
+          service = described_class.new(username: "username", password: "password")
+
+          expect(service).to be_valid
+        end
       end
     end
   end
