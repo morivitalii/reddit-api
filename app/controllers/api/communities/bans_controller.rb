8b502f43a8ed99092c65b8e6d71074213958f2f9
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-28 09:51:00 +0200

    Remove BanDecorator

--- app/controllers/api/communities/bans_controller.rb
+++ app/controllers/api/communities/bans_controller.rb
@@ -3,7 +3,6 @@ class Api::Communities::BansController < ApplicationController
   before_action :set_ban, only: [:edit, :update, :destroy]
   before_action -> { authorize(nil, policy_class: Api::Communities::BansPolicy) }, only: [:index, :new, :create]
   before_action -> { authorize(@ban, policy_class: Api::Communities::BansPolicy) }, only: [:edit, :update, :destroy]
-  decorates_assigned :bans, :ban
 
   def index
     @bans, @pagination = query.paginate(after: params[:after])
--- app/decorators/ban_decorator.rb
+++
@@ -1,5 +0,0 @@
-class BanDecorator < ApplicationDecorator
-  def username
-    model.user.username
-  end
-end
