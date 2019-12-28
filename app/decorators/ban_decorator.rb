6ecb3e06e2ed84fc2bf442c27dd5af8037372130
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-28 02:03:45 +0200

    Remove BanDecorator.details

--- app/decorators/ban_decorator.rb
+++ app/decorators/ban_decorator.rb
@@ -2,18 +2,4 @@ class BanDecorator < ApplicationDecorator
   def username
     model.user.username
   end
-
-  def details
-    created_at = h.t("bans.ban.created_at_html", created_at: model.created_at)
-    type = model.permanent? ? h.t("bans.ban.permanent") : h.t("bans.ban.end_at_html", end_at: model.end_at)
-    reason = model.reason.present? ? h.t("bans.ban.reason", reason: ban.reason) : ""
-
-    h.content_tag(:span) do
-      h.concat(created_at)
-      h.concat " "
-      h.concat(type)
-      h.concat " "
-      h.concat(reason)
-    end
-  end
 end
