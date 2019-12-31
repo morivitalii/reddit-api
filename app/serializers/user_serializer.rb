7a0e7d5967476bf7fbe49888a8a1962752ec0397
Vitaly Igorevich <dev1.vitaly@gmail.com>
2019-12-31 06:00:06 +0200

    Add updated_at to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,6 +4,7 @@ class UserSerializer < ApplicationSerializer
       id: model.id,
       username: model.username,
       created_at: model.created_at,
+      updated_at: model.updated_at
     }
   end
 end
