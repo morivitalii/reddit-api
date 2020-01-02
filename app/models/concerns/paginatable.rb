dc27d2eaece92b7205027e4d2b45fd741cffc5ba
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-02 03:57:23 +0200

    Remove pagination_record from paginatable concern

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -8,26 +8,17 @@ module Paginatable
       limit = options.fetch(:limit, 50)
       pagination_record = options[:after].present? ? unscoped.find_by_id(options[:after]) : nil
 
-      scope = pagination_scope(attributes, order, limit, pagination_record)
-      paginate_scope(scope, limit)
+      pagination_scope(attributes, order, limit, pagination_record).to_a
     end
 
     private
 
     def self.pagination_scope(attributes, order, limit, pagination_record = nil)
-      scope = pagination_limit_scope(limit).pagination_order_scope(attributes, order)
+      order_options = Hash[attributes.map { |attribute| [attribute, order] }]
+
+      scope = limit(limit).order(order_options)
 
       pagination_record.present? ? scope.pagination_after_scope(attributes, order, pagination_record) : scope
-    end
-
-    def self.pagination_limit_scope(limit)
-      limit(limit + 1)
-    end
-
-    def self.pagination_order_scope(attributes, order)
-      options = Hash[attributes.map { |attribute| [attribute, order] }]
-
-      order(options)
     end
 
     def self.pagination_after_scope(attributes, order, pagination_record)
@@ -37,17 +28,6 @@ module Paginatable
       values = pagination_record.attributes.slice(*attributes).values
 
       where("(#{columns}) #{symbol} (#{fillers})", *values)
-    end
-
-    def self.paginate_scope(scope, limit)
-      records = scope.to_a
-
-      if records.size > limit
-        records.delete_at(-1)
-        pagination_record = records.last
-      end
-
-      [records, pagination_record]
     end
   end
 end
--- spec/support/rspec_shared_examples/paginatable_shared_examples.rb
+++ spec/support/rspec_shared_examples/paginatable_shared_examples.rb
@@ -1,34 +1,13 @@
 require "rails_helper"
 
 RSpec.shared_examples "paginatable" do
-  context "when there is records after last one in collection" do
-    it "sets it as a pagination record" do
-      create_list(factory_name, 2)
-
-      records, pagination_record = described_class.all.paginate(limit: 1)
-
-      expect(records.last).to eq(pagination_record)
-    end
-  end
-
-  context "when there is not records after last one in collection" do
-    it "sets pagination record to nil" do
-      create_list(factory_name, 2)
-
-      _, pagination_record = described_class.all.paginate(limit: 2)
-
-      expect(pagination_record).to be_nil
-    end
-  end
-
   context "when :attributes option is provided with multiple attributes" do
     it "paginates by provided attributes" do
       models = create_list(factory_name, 3)
 
-      records, pagination_record = described_class.all.paginate(attributes: [:id, :created_at], limit: 2)
+      records = described_class.all.paginate(attributes: [:id, :created_at], limit: 2)
 
       expect(records).to match_array(models[1..-1])
-      expect(pagination_record).to eq(pagination_record)
     end
   end
 
@@ -37,7 +16,7 @@ RSpec.shared_examples "paginatable" do
       it "orders collection by asc" do
         models = create_list(factory_name, 2)
 
-        records, _ = described_class.all.paginate(order: :asc, limit: 2)
+        records = described_class.all.paginate(order: :asc, limit: 2)
 
         expect(records).to eq(models)
       end
@@ -47,7 +26,7 @@ RSpec.shared_examples "paginatable" do
       it "orders collection by desc" do
         models = create_list(factory_name, 2)
 
-        records, _ = described_class.all.paginate(order: :desc, limit: 2)
+        records = described_class.all.paginate(order: :desc, limit: 2)
 
         expect(records).to eq(models.reverse)
       end
@@ -58,7 +37,7 @@ RSpec.shared_examples "paginatable" do
     it "limits collection" do
       create_list(factory_name, 3)
 
-      records, _ = described_class.all.paginate(limit: 2)
+      records = described_class.all.paginate(limit: 2)
 
       expect(records.size).to eq(2)
     end
@@ -69,7 +48,7 @@ RSpec.shared_examples "paginatable" do
       models = create_list(factory_name, 3)
       after_id = models[2].id
 
-      records, _ = described_class.all.paginate(after: after_id)
+      records = described_class.all.paginate(after: after_id)
 
       expect(records).to match_array(models[0..1])
     end
