41d7aa4faa7caba89c9fb839a3eef0a8367b9d43
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-03 08:59:00 +0200

    Api::Users::Votes::CommentsController transition to api mode

--- app/controllers/api/users/votes/comments_controller.rb
+++ app/controllers/api/users/votes/comments_controller.rb
@@ -3,14 +3,14 @@ class Api::Users::Votes::CommentsController < ApplicationController
   before_action -> { authorize(Api::Users::Votes::CommentsPolicy, @user) }
 
   def index
-    @comments, @pagination = query.paginate(after: params[:after])
+    query = CommentsQuery.new.voted_by_user(@user)
+    query = query.includes(:community, :created_by, :edited_by, :approved_by, :removed_by, post: [:created_by], comment: [:created_by])
+    comments = query.paginate(after: params[:after])
+
+    render json: CommentSerializer.serialize(comments)
   end
 
   private
-
-  def query
-    CommentsQuery.new.voted_by_user(@user).includes(:user, :post, :community)
-  end
 
   def set_user
     @user = UsersQuery.new.with_username(params[:user_id]).take!
