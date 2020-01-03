6b66b1a52a96c7f53038b7ac026d361c76795905
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-03 08:56:40 +0200

    Api::Users::Votes::PostsController transition to api mode

---
+++ spec/requests/api/users/votes/posts_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Api::Users::Votes::PostsController do
+  describe ".index", context: :as_signed_in_user do
+    it "returns posts objects" do
+      create_list(:post, 2, created_by: user)
+
+      get "/api/users/#{user.to_param}/votes/posts.json"
+
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/users/votes/posts_controller/index/200")
+    end
+  end
+end
