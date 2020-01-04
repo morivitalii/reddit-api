e7a8eef4ade11e2e59e4827d1449e20a89874f65
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-04 10:48:16 +0200

    Add /api/users/:user_id/posts/controversy/month.json endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
           namespace :controversy do
             resources :day, only: [:index]
             resources :week, only: [:index]
+            resources :month, only: [:index]
           end
         end
 
