b508ad5699d387c079c4b31a47282c2ed4497a69
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-05 14:32:10 +0200

    Add /api/communities/:community_id/posts/new/month.json endpoint

---
+++ app/controllers/api/communities/posts/new/month_controller.rb
@@ -0,0 +1,23 @@
+class Api::Communities::Posts::New::MonthController < ApplicationController
+  before_action :set_community
+  before_action -> { authorize(Api::Communities::Posts::New::MonthPolicy) }
+
+  def index
+    query = PostsQuery.new(@community.posts).not_removed
+    query = PostsQuery.new(query).for_the_last_month
+    query = query.includes(:community, :created_by, :edited_by, :approved_by, :removed_by)
+    posts = query.paginate(attributes: [:new_score, :id], after: params[:after])
+
+    render json: PostSerializer.serialize(posts)
+  end
+
+  private
+
+  def set_community
+    @community = CommunitiesQuery.new.with_url(params[:community_id]).take!
+  end
+
+  def pundit_user
+    Context.new(current_user, @community)
+  end
+end
---
+++ app/policies/api/communities/posts/new/month_policy.rb
@@ -0,0 +1,5 @@
+class Api::Communities::Posts::New::MonthPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+end
