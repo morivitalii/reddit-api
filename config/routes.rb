50dcb93a641403dd1fb2a4cbd5bff83dfe97196f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-05 10:33:15 +0200

    Add /api/users/:user_id/comments/top/month.json endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
           namespace :top do
             resources :day, only: [:index]
             resources :week, only: [:index]
+            resources :month, only: [:index]
           end
         end
 
---
+++ spec/requests/api/users/comments/top/month_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Api::Users::Comments::Top::MonthController do
+  describe ".index", context: :as_signed_in_user do
+    it "returns comments objects" do
+      create_list(:comment, 2, created_by: user)
+
+      get "/api/users/#{user.to_param}/comments/top/month.json"
+
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/users/comments/top/month_controller/index/200")
+    end
+  end
+end
