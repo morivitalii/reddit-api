3caad7b178f738b48465778a0e60da8ed399d416
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-05 09:21:10 +0200

    Remove CommentsQuery.search_created_after method

--- app/queries/comments_query.rb
+++ app/queries/comments_query.rb
@@ -27,12 +27,6 @@ class CommentsQuery < ApplicationQuery
     relation.where("comments.created_at > ?", datetime)
   end
 
-  def search_created_after(datetime)
-    return relation if datetime.blank?
-
-    created_after(datetime)
-  end
-
   def bookmarked_by_user(user)
     relation.joins(:bookmarks).where(bookmarks: {user: user}).order("bookmarks.id desc")
   end
