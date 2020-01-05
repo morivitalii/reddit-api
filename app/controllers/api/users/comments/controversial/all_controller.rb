ff75f46e98dbecd2cf9a357d08803d06640b6d1d
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-05 13:29:49 +0200

    Add /api/users/:user_id/comments/controversial/all.json endpoint

---
+++ app/controllers/api/users/comments/controversial/all_controller.rb
@@ -0,0 +1,18 @@
+class Api::Users::Comments::Controversial::AllController < ApplicationController
+  before_action :set_user
+  before_action -> { authorize(Api::Users::Comments::Controversial::AllPolicy, @user) }
+
+  def index
+    query = CommentsQuery.new(@user.comments).not_removed
+    query = query.includes(:community, :created_by, :edited_by, :approved_by, :removed_by)
+    comments = query.paginate(attributes: [:controversy_score, :id], after: params[:after])
+
+    render json: CommentSerializer.serialize(comments)
+  end
+
+  private
+
+  def set_user
+    @user = UsersQuery.new.with_username(params[:user_id]).take!
+  end
+end
---
+++ spec/requests/api/users/comments/controversial/all_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Api::Users::Comments::Controversial::AllController do
+  describe ".index", context: :as_signed_in_user do
+    it "returns comments objects" do
+      create_list(:comment, 2, created_by: user)
+
+      get "/api/users/#{user.to_param}/comments/controversial/all.json"
+
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/users/comments/controversial/all_controller/index/200")
+    end
+  end
+end
