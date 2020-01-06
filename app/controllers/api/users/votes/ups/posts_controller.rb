a80f0af2856aa5452a91aaf5431ca2f241178d8f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-06 12:07:09 +0200

    Fix pagination in Api::Users::Votes::Ups::PostsController.index

--- app/controllers/api/users/votes/ups/posts_controller.rb
+++ app/controllers/api/users/votes/ups/posts_controller.rb
@@ -3,9 +3,14 @@ class Api::Users::Votes::Ups::PostsController < ApplicationController
   before_action -> { authorize(Api::Users::Votes::Ups::PostsPolicy, @user) }
 
   def index
-    query = PostsQuery.new.up_voted_by_user(@user)
-    query = query.includes(:community, :created_by, :edited_by, :approved_by, :removed_by)
-    posts = query.paginate(after: params[:after])
+    posts_ids_query = VotesQuery.new(@user.votes).for_posts
+    posts_ids_query = VotesQuery.new(posts_ids_query).up_votes
+    posts_ids_query = posts_ids_query.paginate(after: after)
+    posts_ids = posts_ids_query.map(&:votable_id)
+
+    posts_query = Post.where(id: posts_ids)
+    posts_query = posts_query.includes(:community, :created_by, :edited_by, :approved_by, :removed_by)
+    posts = posts_query.sort_by { |post| posts_ids.index(post.id) }
 
     render json: PostSerializer.serialize(posts)
   end
@@ -14,5 +19,16 @@ class Api::Users::Votes::Ups::PostsController < ApplicationController
 
   def set_user
     @user = UsersQuery.new.with_username(params[:user_id]).take!
+  end
+
+  def after
+    if params[:after].present?
+      query = VotesQuery.new(@user.votes).for_posts
+      query = VotesQuery.new(query).up_votes
+
+      query.where(votable_id: params[:after]).take
+    else
+      nil
+    end
   end
 end
