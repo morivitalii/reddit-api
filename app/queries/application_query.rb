7aeef2e98033e48d526f242eddcc3baa735517df
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-06 11:01:49 +0200

    Add method ApplicationQuery.model_class

--- app/queries/application_query.rb
+++ app/queries/application_query.rb
@@ -4,4 +4,10 @@ class ApplicationQuery
   def initialize(relation = model_class.all)
     @relation = relation
   end
+
+  private
+
+  def model_class
+    self.class.name.chomp("Query").singularize.constantize
+  end
 end
