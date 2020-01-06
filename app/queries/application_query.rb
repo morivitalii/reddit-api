db06cd8368699fa4279755666b9e3e050c1ad60f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-01-06 10:26:48 +0200

    Remove ApplicationQuery.model_class

--- app/queries/application_query.rb
+++ app/queries/application_query.rb
@@ -4,10 +4,4 @@ class ApplicationQuery
   def initialize(relation = model_class.all)
     @relation = relation
   end
-
-  private
-
-  def model_class
-    self.class.name.chomp("Query").singularize.constantize
-  end
 end
