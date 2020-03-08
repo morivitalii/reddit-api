087591767f27b45470da2b6caf0af79c1a990b1b
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-03-08 16:34:14 +0200

    Api::Communities::Posts::Controversial::AllController additional specs

--- spec/requests/api/communities/posts/controversial/all_spec.rb
+++ spec/requests/api/communities/posts/controversial/all_spec.rb
@@ -1,15 +1,79 @@
 require "rails_helper"
 
 RSpec.describe Api::Communities::Posts::Controversial::AllController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
+  describe ".index" do
+    it "returns posts sorted by controversy score" do
       community = create(:community)
-      create_list(:post, 2, community: community)
+      first_post = create(:post, community: community, controversy_score: 2)
+      second_post = create(:post, community: community, controversy_score: 1)
 
       get "/api/communities/#{community.to_param}/posts/controversial/all.json"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/communities/posts/controversial/all_controller/index/200")
+      expect(response).to have_sorted_json_collection(first_post, second_post)
+    end
+
+    it "returns paginated posts" do
+      community = create(:community)
+      first_post = create(:post, community: community, controversy_score: 3)
+      second_post = create(:post, community: community, controversy_score: 2)
+      third_post = create(:post, community: community, controversy_score: 1)
+
+      get "/api/communities/#{community.to_param}/posts/controversial/all.json?after=#{first_post.to_param}"
+
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema("controllers/api/communities/posts/controversial/all_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, third_post)
+    end
+
+    context "for signed out user", context: :as_signed_out_user do
+      it "returns posts" do
+        community = create(:community)
+        first_post = create(:post, community: community, controversy_score: 2)
+        second_post = create(:post, community: community, controversy_score: 1)
+
+        get "/api/communities/#{community.to_param}/posts/controversial/all.json"
+
+        expect(response).to have_http_status(200)
+        expect(response).to match_json_schema("controllers/api/communities/posts/controversial/all_controller/index/200")
+        expect(response).to have_sorted_json_collection(first_post, second_post)
+      end
+    end
+
+    context "for signed in user", context: :as_signed_in_user do
+      it "returns posts" do
+        community = create(:community)
+        first_post = create(:post, community: community, controversy_score: 2)
+        second_post = create(:post, community: community, controversy_score: 1)
+
+        get "/api/communities/#{community.to_param}/posts/controversial/all.json"
+
+        expect(response).to have_http_status(200)
+        expect(response).to match_json_schema("controllers/api/communities/posts/controversial/all_controller/index/200")
+        expect(response).to have_sorted_json_collection(first_post, second_post)
+      end
+    end
+
+    context "for moderator user", context: :as_moderator_user do
+      it "returns posts" do
+        first_post = create(:post, community: context.community, controversy_score: 2)
+        second_post = create(:post, community: context.community, controversy_score: 1)
+
+        get "/api/communities/#{context.community.to_param}/posts/controversial/all.json"
+
+        expect(response).to have_http_status(200)
+        expect(response).to match_json_schema("controllers/api/communities/posts/controversial/all_controller/index/200")
+        expect(response).to have_sorted_json_collection(first_post, second_post)
+      end
+    end
+
+    context "for banned user", context: :as_banned_user do
+      it "returns posts" do
+        get "/api/communities/#{context.community.to_param}/posts/controversial/all.json"
+
+        expect(response).to have_http_status(403)
+      end
     end
   end
 end
