037155d8e9b3b7f0a70606b6cca3c65a407c14ff
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-03-09 09:09:31 +0200

    Add banned role to Api::Communities::Posts::Reports::IgnorePolicy spec

--- app/policies/api/communities/posts/reports/ignore_policy.rb
+++ app/policies/api/communities/posts/reports/ignore_policy.rb
@@ -1,9 +1,9 @@
 class Api::Communities::Posts::Reports::IgnorePolicy < ApplicationPolicy
   def create?
-    moderator?
+    moderator? && !banned?
   end
 
   def destroy?
-    moderator?
+    moderator? && !banned?
   end
 end
