f2bd02469c91eaa4e21048954b6402b4aadf5f7f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-17 01:18:46 +0300

    Remove ModeratorsQuery.with_username method

--- app/queries/moderators_query.rb
+++ app/queries/moderators_query.rb
@@ -1,8 +1,4 @@
 class ModeratorsQuery < ApplicationQuery
-  def with_username(username)
-    relation.joins(:user).where("lower(users.username) = lower(?)", username)
-  end
-
   def recent(limit)
     relation.order(id: :asc).limit(limit)
   end
--- spec/queries/moderators_query_spec.rb
+++ spec/queries/moderators_query_spec.rb
@@ -3,18 +3,6 @@ require "rails_helper"
 RSpec.describe ModeratorsQuery do
   subject { described_class }
 
-  describe ".with_username" do
-    it "returns moderators where user has given username" do
-      user = create(:user)
-      moderators = create_pair(:moderator, user: user)
-      create_pair(:moderator)
-
-      result = subject.new.with_username(user.username)
-
-      expect(result).to match_array(moderators)
-    end
-  end
-
   describe ".recent" do
     it "returns limited recent moderators" do
       moderators = create_list(:moderator, 3)
