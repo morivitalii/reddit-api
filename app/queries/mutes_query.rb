506f1ce4c86bb63563b34e70a8ab0aa6a2719715
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-17 01:19:46 +0300

    Remove MutesQuery.with_username method

--- app/queries/mutes_query.rb
+++ app/queries/mutes_query.rb
@@ -1,8 +1,4 @@
 class MutesQuery < ApplicationQuery
-  def with_username(username)
-    relation.joins(:user).where("lower(users.username) = lower(?)", username)
-  end
-
   def stale
     relation.where("mutes.end_at < ?", Time.current)
   end
--- spec/queries/mutes_query_spec.rb
+++ spec/queries/mutes_query_spec.rb
@@ -3,18 +3,6 @@ require "rails_helper"
 RSpec.describe MutesQuery do
   subject { described_class }
 
-  describe ".with_username" do
-    it "returns mutes with given user username" do
-      user = create(:user)
-      user_mutes = create_pair(:mute, user: user)
-      create_pair(:mute)
-
-      result = subject.new.with_username(user.username)
-
-      expect(result).to match_array(user_mutes)
-    end
-  end
-
   describe ".stale" do
     it "returns stale mutes" do
       stale_mutes = create_pair(:mute, :stale)
