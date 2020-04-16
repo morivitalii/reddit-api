96d5bb7699a39e884ab2ad6a5ac93d4e922efb8e
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-17 01:17:57 +0300

    Remove BansQuery.with_username method

--- app/queries/bans_query.rb
+++ app/queries/bans_query.rb
@@ -1,8 +1,4 @@
 class BansQuery < ApplicationQuery
-  def with_username(username)
-    relation.joins(:user).where("lower(users.username) = lower(?)", username)
-  end
-
   def stale
     relation.where("bans.end_at < ?", Time.current)
   end
--- spec/queries/bans_query_spec.rb
+++ spec/queries/bans_query_spec.rb
@@ -3,18 +3,6 @@ require "rails_helper"
 RSpec.describe BansQuery do
   subject { described_class }
 
-  describe ".with_username" do
-    it "returns bans with given user username" do
-      user = create(:user)
-      user_bans = create_pair(:ban, user: user)
-      create_pair(:ban)
-
-      result = subject.new.with_username(user.username)
-
-      expect(result).to match_array(user_bans)
-    end
-  end
-
   describe ".stale" do
     it "returns stale bans" do
       stale_bans = create_pair(:ban, :stale)
