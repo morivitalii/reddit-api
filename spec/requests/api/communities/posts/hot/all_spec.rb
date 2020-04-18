fc268d7ae66c11c0025972508fa5c5d1f534601b
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-18 19:16:35 +0300

    Api::Communities::Posts::Hot::AllController spec refactoring

--- spec/requests/api/communities/posts/hot/all_spec.rb
+++ spec/requests/api/communities/posts/hot/all_spec.rb
@@ -1,15 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Communities::Posts::Hot::AllController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
+RSpec.describe Api::Communities::Posts::Hot::AllController, context: :as_signed_out_user do
+  describe ".index" do
+    it "returns paginated posts sorted by hot score" do
       community = create(:community)
-      create_list(:post, 2, community: community)
+      first_post = create(:created_last_month_post, community: community, hot_score: 3)
+      second_post = create(:created_last_week_post, community: community, hot_score: 2)
+      third_post = create(:created_yesterday_post, community: community, hot_score: 1)
 
-      get "/api/communities/#{community.to_param}/posts/hot/all.json"
+      get "/api/communities/#{community.to_param}/posts/hot/all.json?after=#{first_post.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/communities/posts/hot/all_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, third_post)
     end
   end
 end
