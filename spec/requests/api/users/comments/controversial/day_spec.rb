f3402bcfe69d5773de6c73f8ddc7fce0f475b86f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-19 15:20:58 +0300

    Api::Users::Comments::Controversial::DayController spec refactoring

--- spec/requests/api/users/comments/controversial/day_spec.rb
+++ spec/requests/api/users/comments/controversial/day_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Comments::Controversial::DayController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment, 2, created_by: context.user)
+RSpec.describe Api::Users::Comments::Controversial::DayController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated daily comments sorted by controversial score" do
+      user = context.user
+      _unrelated_comment = create(:created_yesterday_comment, created_by: user)
+      first_comment = create(:created_today_comment, created_by: user, controversy_score: 3)
+      second_comment = create(:created_today_comment, created_by: user, controversy_score: 2)
+      third_comment = create(:created_today_comment, created_by: user, controversy_score: 1)
 
-      get "/api/users/#{context.user.to_param}/comments/controversial/day.json"
+      get "/api/users/#{user.to_param}/comments/controversial/day.json?after=#{first_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/comments/controversial/day_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, third_comment)
     end
   end
 end
