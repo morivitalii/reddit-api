97d6d1dcd9382da6f2cc084c024b3dcb0d5f0671
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-19 14:26:12 +0300

    Api::Communities::Posts::New::DayController spec refactoring

--- spec/requests/api/communities/posts/new/day_spec.rb
+++ spec/requests/api/communities/posts/new/day_spec.rb
@@ -1,15 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Communities::Posts::New::DayController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
+RSpec.describe Api::Communities::Posts::New::DayController, context: :as_signed_out_user do
+  describe ".index" do
+    it "returns paginated daily posts sorted by new score" do
       community = create(:community)
-      create_list(:post, 2, community: community)
+      _unrelated_post = create(:created_yesterday_post, community: community)
+      first_post = create(:created_today_post, community: community, new_score: 3)
+      second_post = create(:created_today_post, community: community, new_score: 2)
+      third_post = create(:created_today_post, community: community, new_score: 1)
 
-      get "/api/communities/#{community.to_param}/posts/new/day.json"
+      get "/api/communities/#{community.to_param}/posts/new/day.json?after=#{first_post.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/communities/posts/new/day_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, third_post)
     end
   end
 end
