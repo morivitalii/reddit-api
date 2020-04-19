98845f1e5ca90705dfc63e9b7c0b1d609a0bb281
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-19 15:04:00 +0300

    Api::Users::Bookmarks::PostsController spec refactoring

--- spec/requests/api/users/bookmarks/posts_spec.rb
+++ spec/requests/api/users/bookmarks/posts_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Bookmarks::PostsController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
-      create_list(:post_with_bookmark, 2, bookmarked_by: context.user)
+RSpec.describe Api::Users::Bookmarks::PostsController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated bookmarked posts sorted by desc" do
+      user = context.user
+      first_post = create(:post_with_bookmark, bookmarked_by: user)
+      second_post = create(:post_with_bookmark, bookmarked_by: user)
+      third_post = create(:post_with_bookmark, bookmarked_by: user)
 
-      get "/api/users/#{context.user.to_param}/bookmarks/posts.json"
+      get "/api/users/#{user.to_param}/bookmarks/posts.json?after=#{third_post.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/bookmarks/posts_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, first_post)
     end
   end
 end
