b824d63e3c563df6ed0a229b2010230a3e1ada7f
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-19 14:59:40 +0300

    Api::Users::Bookmarks::CommentsController spec refactoring

--- spec/requests/api/users/bookmarks/comments_spec.rb
+++ spec/requests/api/users/bookmarks/comments_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Bookmarks::CommentsController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment_with_bookmark, 2, bookmarked_by: context.user)
+RSpec.describe Api::Users::Bookmarks::CommentsController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated bookmarked comments sorted by desc" do
+      user = context.user
+      first_comment = create(:comment_with_bookmark, bookmarked_by: user)
+      second_comment = create(:comment_with_bookmark, bookmarked_by: user)
+      third_comment = create(:comment_with_bookmark, bookmarked_by: user)
 
-      get "/api/users/#{context.user.to_param}/bookmarks/comments.json"
+      get "/api/users/#{user.to_param}/bookmarks/comments.json?after=#{third_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/bookmarks/comments_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, first_comment)
     end
   end
 end
