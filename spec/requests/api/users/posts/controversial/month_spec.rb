7c39a5c7e97c39823d58eceda6f1496c5bfba9c5
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 13:58:39 +0300

    Api::Users::Posts::Controversial::MonthController spec refactoring

--- spec/requests/api/users/posts/controversial/month_spec.rb
+++ spec/requests/api/users/posts/controversial/month_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Posts::Controversial::MonthController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
-      create_list(:post, 2, created_by: context.user)
+RSpec.describe Api::Users::Posts::Controversial::MonthController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated monthly posts sorted by controversial score" do
+      user = context.user
+      _unrelated_post = create(:created_last_month_post, created_by: user)
+      first_post = create(:created_this_month_post, created_by: user, controversy_score: 3)
+      second_post = create(:created_this_month_post, created_by: user, controversy_score: 2)
+      third_post = create(:created_this_month_post, created_by: user, controversy_score: 1)
 
-      get "/api/users/#{context.user.to_param}/posts/controversial/month.json"
+      get "/api/users/#{user.to_param}/posts/controversial/month.json?after=#{first_post.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/posts/controversial/month_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, third_post)
     end
   end
 end
