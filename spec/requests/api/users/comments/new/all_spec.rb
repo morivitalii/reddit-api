36c14c1e3a994c84ca1e7eee0c10fb626ca00fa9
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 13:49:15 +0300

    Api::Users::Comments::New::AllController spec refactoring

--- spec/requests/api/users/comments/new/all_spec.rb
+++ spec/requests/api/users/comments/new/all_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Comments::New::AllController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment, 2, created_by: context.user)
+RSpec.describe Api::Users::Comments::New::AllController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated comments sorted by new score" do
+      user = context.user
+      first_comment = create(:created_last_month_comment, created_by: user, new_score: 3)
+      second_comment = create(:created_last_week_comment, created_by: user, new_score: 2)
+      third_comment = create(:created_yesterday_comment, created_by: user, new_score: 1)
 
-      get "/api/users/#{context.user.to_param}/comments/new/all.json"
+      get "/api/users/#{user.to_param}/comments/new/all.json?after=#{first_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/comments/new/all_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, third_comment)
     end
   end
 end
