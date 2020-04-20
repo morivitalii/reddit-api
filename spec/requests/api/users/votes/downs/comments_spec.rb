ec5d7591e2b17a839dad649b09bd3cf7121c427a
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 21:26:32 +0300

    Api::Users::Votes::Downs::CommentsController spec refactoring

--- spec/requests/api/users/votes/downs/comments_spec.rb
+++ spec/requests/api/users/votes/downs/comments_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Votes::Downs::CommentsController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment_with_down_vote, 2, created_by: context.user)
+RSpec.describe Api::Users::Votes::Downs::CommentsController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated down voted comments sorted by desc" do
+      user = context.user
+      first_comment = create(:comment_with_down_vote, voted_by: user)
+      second_comment = create(:comment_with_down_vote, voted_by: user)
+      third_comment = create(:comment_with_down_vote, voted_by: user)
 
-      get "/api/users/#{context.user.to_param}/votes/downs/comments.json"
+      get "/api/users/#{user.to_param}/votes/downs/comments.json?after=#{third_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/votes/downs/comments_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, first_comment)
     end
   end
 end
