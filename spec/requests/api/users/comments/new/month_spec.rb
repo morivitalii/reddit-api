8ebc62b470a469b9f52842cd4271b73f68f9e933
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 13:49:49 +0300

    Api::Users::Comments::New::MonthController spec refactoring

--- spec/requests/api/users/comments/new/month_spec.rb
+++ spec/requests/api/users/comments/new/month_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Comments::New::MonthController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment, 2, created_by: context.user)
+RSpec.describe Api::Users::Comments::New::MonthController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated monthly comments sorted by new score" do
+      user = context.user
+      _unrelated_comment = create(:created_last_month_comment, created_by: user)
+      first_comment = create(:created_this_month_comment, created_by: user, new_score: 3)
+      second_comment = create(:created_this_month_comment, created_by: user, new_score: 2)
+      third_comment = create(:created_this_month_comment, created_by: user, new_score: 1)
 
-      get "/api/users/#{context.user.to_param}/comments/new/month.json"
+      get "/api/users/#{user.to_param}/comments/new/month.json?after=#{first_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/comments/new/month_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, third_comment)
     end
   end
 end
