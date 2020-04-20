35109f9f81cafb3a005e96a270ef52cbeeac865e
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 13:52:39 +0300

    Api::Users::Comments::Top::WeekController spec refactoring

--- spec/requests/api/users/comments/top/week_spec.rb
+++ spec/requests/api/users/comments/top/week_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Comments::Top::WeekController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment, 2, created_by: context.user)
+RSpec.describe Api::Users::Comments::Top::WeekController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated weekly comments sorted by top score" do
+      user = context.user
+      _unrelated_comment = create(:created_last_week_comment, created_by: user)
+      first_comment = create(:created_this_week_comment, created_by: user, top_score: 3)
+      second_comment = create(:created_this_week_comment, created_by: user, top_score: 2)
+      third_comment = create(:created_this_week_comment, created_by: user, top_score: 1)
 
-      get "/api/users/#{context.user.to_param}/comments/top/week.json"
+      get "/api/users/#{user.to_param}/comments/top/week.json?after=#{first_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/comments/top/week_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, third_comment)
     end
   end
 end
