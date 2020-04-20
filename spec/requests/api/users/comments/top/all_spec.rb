ffe69fb07fdbf100736995b8c01c72089118d125
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 13:50:28 +0300

    Api::Users::Comments::Top::AllController spec refactoring

--- spec/requests/api/users/comments/top/all_spec.rb
+++ spec/requests/api/users/comments/top/all_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Comments::Top::AllController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns comments objects" do
-      create_list(:comment, 2, created_by: context.user)
+RSpec.describe Api::Users::Comments::Top::AllController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated comments sorted by top score" do
+      user = context.user
+      first_comment = create(:created_last_month_comment, created_by: user, top_score: 3)
+      second_comment = create(:created_last_week_comment, created_by: user, top_score: 2)
+      third_comment = create(:created_yesterday_comment, created_by: user, top_score: 1)
 
-      get "/api/users/#{context.user.to_param}/comments/top/all.json"
+      get "/api/users/#{user.to_param}/comments/top/all.json?after=#{first_comment.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/comments/top/all_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_comment, third_comment)
     end
   end
 end
