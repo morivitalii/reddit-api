b613980255b077a63bbc5b9df4900519cddc225e
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 23:20:55 +0300

    Communities::Posts::Comments::DeleteDownVote refactoring

--- app/services/communities/posts/comments/delete_down_vote.rb
+++ app/services/communities/posts/comments/delete_down_vote.rb
@@ -1,10 +1,7 @@
 class Communities::Posts::Comments::DeleteDownVote
-  attr_accessor :comment, :user
+  include ActiveModel::Model
 
-  def initialize(comment, user)
-    @comment = comment
-    @user = user
-  end
+  attr_accessor :comment, :user
 
   def call
     return false if vote.blank?
