b8f7d854fa31a55f647198a971308bfa65d9fafe
Vitaly Igorevich <dev1.vitaly@gmail.com>
2020-04-20 14:01:45 +0300

    Api::Users::Posts::Hot::WeekController spec refactoring

--- spec/requests/api/users/posts/hot/week_spec.rb
+++ spec/requests/api/users/posts/hot/week_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
 
-RSpec.describe Api::Users::Posts::Hot::WeekController do
-  describe ".index", context: :as_signed_in_user do
-    it "returns posts objects" do
-      create_list(:post, 2, created_by: context.user)
+RSpec.describe Api::Users::Posts::Hot::WeekController, context: :as_signed_in_user do
+  describe ".index" do
+    it "returns paginated weekly posts sorted by hot score" do
+      user = context.user
+      _unrelated_post = create(:created_last_week_post, created_by: user)
+      first_post = create(:created_this_week_post, created_by: user, hot_score: 3)
+      second_post = create(:created_this_week_post, created_by: user, hot_score: 2)
+      third_post = create(:created_this_week_post, created_by: user, hot_score: 1)
 
-      get "/api/users/#{context.user.to_param}/posts/hot/week.json"
+      get "/api/users/#{user.to_param}/posts/hot/week.json?after=#{first_post.to_param}"
 
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema("controllers/api/users/posts/hot/week_controller/index/200")
+      expect(response).to have_sorted_json_collection(second_post, third_post)
     end
   end
 end
